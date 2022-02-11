2ce5520c7277c7c4007338e2ad5cb8386fafc9b0
Mohit <mohitdulani28@gmail.com>
2022-02-11 12:17:56 +0530

    I have completed basic use of api

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import React , {useEffect} from 'react'
+import noteContext from '../context/notes/noteContext'
+
+
+const About = () => {
+  
+  return (
+    <>
+      <div className='container'>
+        This is about js file
+        </div>
+    </>
+  )
+}
+
+export default About
